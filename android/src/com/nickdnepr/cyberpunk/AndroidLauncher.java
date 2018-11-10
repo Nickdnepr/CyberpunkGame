eee93b11f5de21e6263bdbf02d2816b7dc27191b
nick <nick.dnepr@gmail.com>
2018-11-10 23:12:20 +0200

    - initial commit

---
+++ android/src/com/nickdnepr/cyberpunk/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.nickdnepr.cyberpunk;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.nickdnepr.cyberpunk.CyberPunk;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CyberPunk(), config);
+	}
+}
